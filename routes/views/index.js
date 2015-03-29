95b7bd4801007c9547681762c826a0454c7e6c84
Ply_py <me@plypy.com>
2015-03-29 14:36:05 +0800

    add new-task bar

--- routes/views/index.js
+++ routes/views/index.js
@@ -4,6 +4,11 @@ exports = module.exports = function(req, res) {
 
   var view = new keystone.View(req, res);
 
-  view.render('index');
+  view.render('index', {
+    tasks: [
+      {name: 'Fairy', id: '123', remain: 10, percentage: 60},
+      {name: 'Lucy', id: '321', remain: 2, percentage: 20},
+    ]
+  });
 
 }
